6322087ee95ecd7abd30d7c903dce346a097f3b7
Maximus7474 <the.maximus.7474@gmail.com>
2025-08-17 13:23:34 +0200

    doc(server/lb-tablet): document regions and configuration

--- server/sv_lbtablet_compat.lua
+++ server/sv_lbtablet_compat.lua
@@ -12,17 +12,36 @@ end)
 AddEventHandler('onResourceStop', function (resource)
     if (resource == "lb-tablet") then lbtabletState = "stopped" end
 end)
+
+--[[ Configuration ]]
+-- These are the statuses you want the ALPR to trigger,
+-- They are the keys listed in the lb-tablet config:
+-- Config.Police.WarrantStatuses = {
+--     active = {
+--         color = "red",
+--         label = "Active"
+--     },
+--     cancelled = {
+--         color = "orange",
+--         label = "Cancelled"
+--     },
+--     expired = {
+--         color = "red",
+--         label = "Expired"
+--     },
+-- }
 local activeWarrantStatuses = {
     "active"
 }
-local searchParams = ""
 
+--[[ Don't touch ]]
+local searchParams = ""
 for i = 1, #activeWarrantStatuses do
     activeWarrantStatuses[i] = ("`warrant_status` = '%s'"):format(activeWarrantStatuses[i])
 end
-
 searchParams = table.concat(activeWarrantStatuses, ' AND ')
 
+---Server handler to check if the tablet has an active warrant
 ---@param cam string ALPR camera index
 ---@param plate string license text
 ---@param index number plate texture index
@@ -41,7 +60,7 @@ RegisterNetEvent('wk:onPlateScanned', function (cam, plate, index)
         plate
     })
 
-    if not response then return end
+    if (not response) then return end
 
     TriggerClientEvent(
         "wk:togglePlateLock", src,
